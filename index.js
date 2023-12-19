22f22c6b5975f423b4e440556bac43b53bbb0ccf
kpilszak <klaudia.pilszak@gmail.com>
2023-12-19 15:03:19 -0500

    Get data from theguardian web page

--- index.js
+++ index.js
@@ -5,4 +5,16 @@ const cheerio = require('cheerio');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.json('Welcome to Climate Change News API');
+});
+
+app.get('/news', (req, res) => {
+    axios.get('https://theguardian.com/environment/climate-crisis')
+        .then((response) => {
+            const html = response.data;
+            console.log(html);
+        })
+});
+
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
