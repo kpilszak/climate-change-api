5559d72c78446437c26f8f9fd26f78fd99283f90
kpilszak <klaudia.pilszak@gmail.com>
2023-12-19 15:09:13 -0500

    Scrape for climate titles

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cheerio = require('cheerio');
 
 const app = express();
 
+const articles = [];
+
 app.get('/', (req, res) => {
     res.json('Welcome to Climate Change News API');
 });
@@ -14,7 +16,16 @@ app.get('/news', (req, res) => {
         .then((response) => {
             const html = response.data;
             const $ = cheerio.load(html);
-        })
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text();
+                const url = $(this).attr('href');
+                articles.push({
+                    title,
+                    url
+                })
+            });
+            res.json(articles);
+        }).catch((err) => console.log(err));
 });
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
